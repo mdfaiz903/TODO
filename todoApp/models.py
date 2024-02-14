9f9340ef6be4a906c97a7481bae86d14e1006d22
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-14 12:18:00 +0600

    model cration and templates config. done .

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class todo_model(models.Model):
+    title = models.CharField(max_length = 50)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
--- todoApp/views.py
+++ todoApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello Todo APP")
+    return render(request,'todoApp/index.html')
