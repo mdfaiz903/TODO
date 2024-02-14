d0dcd6df37daa68f687bafe4ca528354869dd667
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-14 14:16:20 +0600

    data creation from django form Done

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render,HttpResponse
 from  . forms import todoForm
+from . models import todo_model
+from django .contrib import messages
 # Create your views here.
 def home(request):
-    form = todoForm()
+    if request.method =='POST':
+        form = todoForm(request.POST)
+        try:
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Items Added Successfully")
+
+        except:
+            raise ValueError("Invalid or Empty Fields")
+    else:
+        form = todoForm()
 
 
     return render(request,'todoApp/index.html',{'frm':form})
