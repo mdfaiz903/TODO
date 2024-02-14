ef7f90ecc0bf526a292cae60a0787de26ea6847d
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-14 23:53:30 +0600

    data update/edit functionality added

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -1,7 +1,8 @@
-from . views import home,deleteview
+from . views import home,deleteview,updateview
 from django.urls import path
 
 urlpatterns = [
     path('',home,name='home'),
     path('delete-<int:id>/',deleteview,name='deleteview'),
+    path('update-<int:id>/',updateview,name='update'),
 ]
